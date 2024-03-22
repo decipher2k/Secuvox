8f59bbe2128ced654e3ff2ea7f95ee1962a2923b
decipher <dennis.heine@gmx.de>
2024-03-22 08:36:16 +0100

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Secuvox_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(2124, 1365);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            this.webView21.Click += new System.EventHandler(this.webView21_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton3,
+            this.toolStripSeparator1,
+            this.toolStripButton2,
+            this.toolStripTextBox1,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(2124, 38);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(34, 33);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 33);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(500, 33);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton4.Text = "toolStripButton4";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(2124, 1365);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.webView21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using WebView2.DevTools.Dom;
+using WebView2.DevTools.Dom.Input;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Secuvox_2._0
+{
+    public partial class Form1 : Form
+    {
+       public Form1()
+        {
+            InitializeComponent();
+
+            if (!System.IO.Directory.Exists(".\\cache"))
+                System.IO.Directory.CreateDirectory(".\\cache");
+
+            webView21.NavigationStarting += WebView21_NavigationStarting;
+            webView21.NavigationCompleted += WebView21_NavigationCompleted;
+            webView21.CoreWebView2InitializationCompleted += WebView21_CoreWebView2InitializationCompleted;
+            webView21.EnsureCoreWebView2Async();
+          
+
+            adblock = System.IO.File.ReadAllLines(".\\hosts");
+            
+            
+           // webView21.CoreWebView2.Settings.UserAgent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
+        }
+
+        private void WebView21_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            webView21.CoreWebView2.WebResourceRequested += CoreWebView2_WebResourceRequested;
+         
+                webView21.CoreWebView2.AddWebResourceRequestedFilter("*", 0);
+       
+        }
+
+        private void CoreWebView2_WebResourceRequested(object sender, Microsoft.Web.WebView2.Core.CoreWebView2WebResourceRequestedEventArgs e)
+        {
+            String[] parts = new Uri(e.Request.Uri).Host.Split('.');
+            if(parts.Length>1)
+                if (adblock.Contains(parts[parts.Length - 2] + "." + parts[parts.Length - 1]))
+                    e.Request.Uri = "about:blank";
+        }
+
+        String[] adblock;
+
+        String[] toReplace = new string[]
+        {
+            
+           // "on",
+            "onmouseenter",
+"onmouseleave",   
+"onmousemove",
+"onmouseout",
+"onmouseover",
+"onmouseup",        
+"mouseleave",
+"mousemove",
+"mouseout",
+"mouseover",
+"mouseup",
+"hover",
+"onhover",
+"onwheel",
+"wheel",
+"scroll",
+"onscroll",
+"scrolled",
+"onscrolled"
+        };
+
+        async void WebView21_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            WebView2DevToolsContext devToolsContext;
+            devToolsContext = await webView21.CoreWebView2.CreateDevToolsContextAsync();
+            String text = await devToolsContext.GetContentAsync();
+            WebView2.DevTools.Dom.HtmlElement[] scripts = await devToolsContext.QuerySelectorAllAsync("script");
+
+
+
+            foreach (WebView2.DevTools.Dom.HtmlElement script in scripts)
+            {
+
+                String sTextOrig = await script.GetInnerTextAsync();
+                String sText = sTextOrig;
+                bool found = true;
+
+                foreach (String s in toReplace)
+                {
+                    sText = sText.Replace("\""+s+"\"", "");
+                    sText = sText.Replace("'" + s + "'", "");
+                    sText = sText.Replace("\"on\"+", "\"no\"+");
+                    sText = sText.Replace("\"on\" +", "\"no\" +");
+                    sText = sText.Replace("'on'+", "\"no\"+");
+                    sText = sText.Replace("'on' +", "\"no\" +");
+
+                }
+
+
+
+
+                /* if ((sTextOrig.Contains("\"click\"") || sTextOrig.Contains("\"onclick\"") || sTextOrig.Contains("\"message\"") || sTextOrig.Contains("\"onmessage\"")))
+
+                 {
+                     found = false;
+                     int start = sTextOrig.IndexOf("attachEvent");
+
+                     int end = start;
+                     while (sTextOrig[end] != 59 && sTextOrig[end] != 38 && sTextOrig[end] != 124)
+                     {
+                         end++;
+                     }
+
+                     if (sTextOrig.Substring(start, end - start).Contains("\"click\"") || sTextOrig.Substring(start, end - start).Contains("\"onclick\"") || sTextOrig.Substring(start, end - start).Contains("\"message\"") || sTextOrig.Substring(start, end - start).Contains("\"onmessage\""))
+                     {
+                         sTextOrig = sTextOrig.Replace(sTextOrig.Substring(start, end - start), "");
+
+                     }
+
+                 }
+                 found = true;
+                 while (sTextOrig.Contains("attachEventListener") && (sTextOrig.Contains("\"click\"") || sTextOrig.Contains("\"onclick\"") || sTextOrig.Contains("\"message\"") || sTextOrig.Contains("\"onmessage\"")))
+                 {
+                     found = false;
+                     int start = sTextOrig.IndexOf("attachEventListener");
+
+                     int end = start;
+                     while (sTextOrig[end] != 59 && sTextOrig[end] != 38 && sTextOrig[end] != 124)
+                     {
+                         end++;
+                     }
+
+                     if (sTextOrig.Substring(start, end - start).Contains("\"click\"") || sTextOrig.Substring(start, end - start).Contains("\"onclick\"") || sTextOrig.Substring(start, end - start).Contains("\"message\"") || sTextOrig.Substring(start, end - start).Contains("\"onmessage\""))
+                     {
+                         sTextOrig = sTextOrig.Replace(sTextOrig.Substring(start, end - start), "");
+
+                     }
+
+                 }
+                 found = true;
+                 while (sTextOrig.Contains("addEventListener") &&  (sTextOrig.Contains("\"click\"") || sTextOrig.Contains("\"onclick\"") || sTextOrig.Contains("\"message\"") || sTextOrig.Contains("\"onmessage\"")))
+                 {
+                     found = false;
+                     int start = sTextOrig.IndexOf("addEventListener");
+
+                     int end = start;
+                     while (sTextOrig[end] != 59 && sTextOrig[end] != 38 && sTextOrig[end] != 124)
+                     {
+                         end++;
+                     }
+
+                     if (sTextOrig.Substring(start, end - start).Contains("\"click\"") || sTextOrig.Substring(start, end - start).Contains("\"onclick\"") || sTextOrig.Substring(start, end - start).Contains("\"message\"") || sTextOrig.Substring(start, end - start).Contains("\"onmessage\""))
+                     {
+                         sTextOrig = sTextOrig.Replace(sTextOrig.Substring(start, end - start), "");
+
+                     }
+
+                 }
+
+                 found = true;
+                 while (sTextOrig.Contains("addEvent") && (sTextOrig.Contains("\"click\"") || sTextOrig.Contains("\"onclick\"") || sTextOrig.Contains("\"message\"") || sTextOrig.Contains("\"onmessage\"")))
+                 {
+                     found = false;
+                     int start = sTextOrig.IndexOf("addEvent");
+
+                     int end = start;
+                     while (sTextOrig[end] != 59 && sTextOrig[end] != 38 && sTextOrig[end] != 124)
+                     {
+                         end++;
+                     }
+
+                     if (sTextOrig.Substring(start, end - start).Contains("\"click\"") || sTextOrig.Substring(start, end - start).Contains("\"onclick\"") || sTextOrig.Substring(start, end - start).Contains("\"message\"") || sTextOrig.Substring(start, end - start).Contains("\"onmessage\""))
+                     if (sTextOrig.Substring(start, end - start).Contains("\"click\"") || sTextOrig.Substring(start, end - start).Contains("\"onclick\"") || sTextOrig.Substring(start, end - start).Contains("\"message\"") || sTextOrig.Substring(start, end - start).Contains("\"onmessage\""))
+                     {
+                         sTextOrig = sTextOrig.Replace(sTextOrig.Substring(start, end - start), "");
+
+                     }
+
+                 }*/
+
+                if (sText != "")
+                    text = text.Replace(sTextOrig, sText);
+            }
+         
+            
+            await devToolsContext.SetContentAsync(text);
+
+        }
+
+        private void WebView21_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+       
+        }
+
+        private String genBodyTag(String bodyTag,  string javascript)
+        {
+            bodyTag = bodyTag + "<script type='text/javascript'>"+javascript+"</script>";
+            return bodyTag;
+        }
+
+  
+
+        private string ReplaceBlockedFunctions(string javascript)
+        {
+            return javascript;
+        }
+
+        private string findSubStrings(string javascript)
+        {
+            return "";
+        }
+
+        private void webView21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            webView21.CoreWebView2.Navigate(toolStripTextBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
